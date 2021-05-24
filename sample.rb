55a950aa43b5bbbba7fbdee7ee159ca5c9f35b56
riri3 <kamijooor@gmail.com>
2021-05-24 12:43:36 +0000

    [2.5.3]

--- sample.rb
+++ sample.rb
@@ -1,46 +1,82 @@
-# <2.4>数値
+# <2.5>真偽値と条件分岐
+# nilまたはfalseであれば偽！nilも偽であることがポイント。
+data = 1
+if data != nil
+  puts 'データがあります'
+else
+  puts 'データがありません'
+end
 
-# _は表示されない。大きな数値を区切るのに便利。
-puts 1_000_000_000
+# 以下はnilの省略版。上と同じ意味。
+data = 2
+if data
+  puts 'データがあります'
+else
+  puts 'データがありません'
+end
 
-n = 1
-puts -n
 
-# 整数同士の割り算は整数になり、小数点以下は切り捨て
-puts 1 / 2
-puts 1.0 / 2
+# 条件１ && 条件２　条件１も２も真であれば真。それ以外は偽。
+t1 = true
+t2 = true
+f1 = false
+puts t1 && t2
+puts t1 && f1
 
-# 変数に整数が入っている場合、to_f(f=float)メソッドを用いて整数→小数へ変更できる
-n = 1
-puts n.to_f
-puts n.to_f / 2
+# 条件１ || 条件２　条件１か２、いずれかが真であれば真。両方偽であれば偽。
+t1 = true
+t2 = true
+f1 = false
+f2 = false
+puts t1 || t2
+puts t1 || f1
+puts f1 || f2
 
-# %は割り算の余りを求める
-puts 8 % 3
+# &&と||では&&の方が優先順位が高い。(p.28参照)
+puts t1 && t2 || f1 && f2
+# 上記は(t1 && t2) || (f1 && f2)というルールで評価される。
 
-# **はべき乗（n=正の整数であればaのn乗 参照：https://atarimae.biz/archives/20521）
-puts 2 ** 3
 
-# 比較
-puts 2 <= 4
-puts 5 >= 4
-puts 0 != 0
-puts 1 == 1
+# 条件１&& (条件２||条件３) && 条件４
+puts t1 && (t2 || f1) && f2
 
-# 以下の２つは同じ。いつも通り、掛ける割るが優先された後,足す引くがくる。
-puts 2 * 3 + 4 * 5 - 6 / 2
-puts (2 * 3) + (4 * 5) - (6 / 2)
+# !を使うと真偽値を反転させる
+puts !t1
+puts !f1
+puts !(t1 && f1)
 
-# 特殊な記載方法
-# nを１増やす（n=n+1と同義）
-n = 1
-puts n += 1
-# nを１減らす（n=n-1と同義）
-puts n -= 1
+# if文
+country = 'italy'
+if country == 'japan'
+  puts 'こんにちは'
+elsif country == 'us'
+  puts 'Hi'
+elsif country == 'italy'
+  puts 'ciao'
+else
+  puts '?'
+end
 
-# xを３倍にする（y=y*3と同義）
- y = 12
-puts y *= 3
-# xを４で割る（z=z/4と同義）
-puts z = 12 
-puts y /= 4
+greeting =
+ if country == 'japan'
+   puts 'こんにちは'
+ elsif country == 'us'
+   puts 'Hi'
+ elsif country == 'italy'
+   puts 'ciao'
+ else
+   puts '?'
+ end
+ 
+puts greeting
+
+
+
+point = 7
+day = 1
+# １日であればポイント５倍
+if day == 1
+  point *= 5
+end
+
+puts point
