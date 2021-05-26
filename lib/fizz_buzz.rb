2e9b6dcab925f8fb06fa0c107d621cdbb161bb38
riri3 <kamijooor@gmail.com>
2021-05-26 12:51:26 +0000

    [3.3.1]

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -10,9 +10,23 @@ def fizz_buzz(n)
   end
 end
 
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(5)
-puts fizz_buzz(7)
-puts fizz_buzz(15)
+# puts fizz_buzz(1)
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(15)
+
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'Fizz Buzz', fizz_buzz(15)
+
+  end
+end
---
+++ lib/sample_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+
+# ここからテストの本体（テストクラス）
+class SampleTest < Minitest::Test
+   def test_sample
+     assert_equal 'RUBY', nil.upcase
+   end
+end
