fb74c6a3df47a9cf135988401bb93398ff8bc459
riri3 <kamijooor@gmail.com>
2021-06-28 14:45:49 +0000

    update

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,14 @@
 def fizz_buzz(n)
+  # １５で割り切れる場合
   if n % 15 == 0
     'Fizz Buzz'
+    # ３で割り切れる場合
   elsif n % 3 == 0
     'Fizz'
+    # ５で割り切れる場合
   elsif n % 5 == 0
     'Buzz'
+    # ３でも５でも割り切れない場合
   else
      n.to_s
   end
